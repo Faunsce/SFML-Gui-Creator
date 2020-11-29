fb595f0f90e00b83b92b1633217e078608d4c3cf
Dominik Rutter <dominik.rutter@gmail.com>
2020-11-29 03:02:10 -0500

    Allow position of objects by mouse

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,75 @@
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Ur mom", sf::Style::Default);
-	sf::RectangleShape physicsTestBox(sf::Vector2f(100, 100));
 	sf::Event evnt;
-	enum DIRECTION {LEFT, RIGHT};
-	DIRECTION direction = LEFT;
-	float speed = 10.0f;
 	sf::Clock programClock;
+
+	std::vector<sf::RectangleShape> boxes{
+		sf::RectangleShape(sf::Vector2f(100, 100)),
+		sf::RectangleShape(sf::Vector2f(100, 100))
+	};
+	sf::RectangleShape* activeBox = nullptr;
+
 	while (window.isOpen()) {
 		long double elapsedTime = programClock.restart().asSeconds();
 		// Input
 		while (window.pollEvent(evnt)) {
 			switch (evnt.type)
 			{
-			case sf::Event::Closed:
+			case sf::Event::Closed: {
 				window.close();
 				break;
+			}
+			case sf::Event::KeyPressed: {
+				switch (evnt.key.code)
+				{
+				case sf::Keyboard::Escape: {
+					window.close();
+				}
+				default:
+					break;
+				}
+			}
+			case sf::Event::MouseButtonPressed: {
+				sf::Vector2f mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition());
+				if (evnt.mouseButton.button == sf::Mouse::Left) {
+					if (activeBox != nullptr) {
+						activeBox->setPosition(mousePos);
+					}
+				}
+				else if (evnt.mouseButton.button == sf::Mouse::Right) {
+					bool boxHit = false;
+					for (auto& box : boxes) {
+						if (box.getGlobalBounds().contains(mousePos)) {
+							if (activeBox != nullptr) {
+								activeBox->setFillColor(sf::Color::White);
+							}
+							activeBox = &box;
+							activeBox->setFillColor(sf::Color::Red);
+							boxHit = true;
+							break;
+						}
+					}
+					if (!boxHit) {
+						activeBox->setFillColor(sf::Color::White);
+						activeBox = nullptr;
+					}
+				}
+				break;
+			}
 			default:
 				break;
 			}
 		}
 		// Logic
-		if (physicsTestBox.getPosition().x > sf::VideoMode::getDesktopMode().width - 100.0f) {
-			direction = DIRECTION::LEFT;
-		} else if (physicsTestBox.getPosition().x < 100.0f) {
-			direction = DIRECTION::RIGHT;
-		}
-		if (direction == DIRECTION::LEFT) {
-			physicsTestBox.move(sf::Vector2f(-speed * elapsedTime, 0.0f));
-		} else if (direction == DIRECTION::RIGHT) {
-			physicsTestBox.move(sf::Vector2f(speed * elapsedTime, 0.0f));
-		}
+
 		// Draw
 		window.clear();
-		window.draw(physicsTestBox);
+		for (const auto& box : boxes) {
+			window.draw(box);
+		}
 		window.display();
 	}
 }
