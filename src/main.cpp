f39091fad7f75c31259912a592d0bda7289679d6
Dominik Rutter <dominik.rutter@gmail.com>
2020-11-29 02:35:59 -0500

    calculate elapsed time in one spot only

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Ur mom", sf::Style::Default);
-	sf::RectangleShape testBox(sf::Vector2f(100, 100));
+	sf::RectangleShape physicsTestBox(sf::Vector2f(100, 100));
 	sf::Event evnt;
 	enum DIRECTION {LEFT, RIGHT};
 	DIRECTION direction = LEFT;
 	float speed = 10.0f;
 	sf::Clock programClock;
 	while (window.isOpen()) {
+		long double elapsedTime = programClock.restart().asSeconds();
 		// Input
 		while (window.pollEvent(evnt)) {
 			switch (evnt.type)
@@ -21,19 +22,19 @@ int main() {
 			}
 		}
 		// Logic
-		if (testBox.getPosition().x > sf::VideoMode::getDesktopMode().width - 100.0f) {
+		if (physicsTestBox.getPosition().x > sf::VideoMode::getDesktopMode().width - 100.0f) {
 			direction = DIRECTION::LEFT;
-		} else if (testBox.getPosition().x < 100.0f) {
+		} else if (physicsTestBox.getPosition().x < 100.0f) {
 			direction = DIRECTION::RIGHT;
 		}
 		if (direction == DIRECTION::LEFT) {
-			testBox.move(sf::Vector2f(-speed * programClock.restart().asSeconds(), 0.0f));
+			physicsTestBox.move(sf::Vector2f(-speed * elapsedTime, 0.0f));
 		} else if (direction == DIRECTION::RIGHT) {
-			testBox.move(sf::Vector2f(speed * programClock.restart().asSeconds(), 0.0f));
+			physicsTestBox.move(sf::Vector2f(speed * elapsedTime, 0.0f));
 		}
 		// Draw
 		window.clear();
-		window.draw(testBox);
+		window.draw(physicsTestBox);
 		window.display();
 	}
 }
