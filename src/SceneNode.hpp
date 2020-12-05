3ccaab5b6086d1540c70c545afb4ddb8ef2ee2d9
Dominik Rutter <dominik.rutter@gmail.com>
2020-12-05 05:48:48 -0500

    make SceneNode::getWorldTransform public

--- src/SceneNode.hpp
+++ src/SceneNode.hpp
@@ -12,12 +12,12 @@ namespace faun
 		void addChild(std::unique_ptr<SceneNode>);
 		std::unique_ptr<SceneNode> removeChild(const SceneNode&);
 		void update(double long);
+		sf::Vector2f getWorldPosition() const;
 	private:
 		virtual void updateSelf(double long);
 		virtual void draw(sf::RenderTarget&, sf::RenderStates) const final;
 		virtual void drawSelf(sf::RenderTarget&, sf::RenderStates) const;
 		sf::Transform getWorldTransform() const;
-		sf::Vector2f getWorldPosition() const;
 		std::vector<std::unique_ptr<SceneNode>> children;
 		SceneNode* parent;
 	};
